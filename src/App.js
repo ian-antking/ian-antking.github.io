18b1072176ba4b9779eb1d474e3e3fe5607d88c3
ian-antking <ian.antking@gmail.com>
2020-08-02 23:05:48 +0100

    add navbar, router and switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import themes from './themes';
@@ -10,13 +11,19 @@ const AppContainer = styled.div`
 `;
 
 function App() {
-  const [theme] = useState('forest');
+  const [theme] = useState('default');
 
   return (
     <ThemeProvider theme={themes[theme]}>
       <AppContainer className="App">
         <GlobalStyle />
         <Nav />
+        <Switch>
+          <Route path="/about" component={() => <h1>About</h1>} />
+          <Route path="/projects" component={() => <h1>Projects</h1>} />
+          <Route path="/contact" component={() => <h1>Contact</h1>} />
+          <Route path="/" exact component={() => <h1>Home</h1>} />
+        </Switch>
       </AppContainer>
     </ThemeProvider>
   );
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const HeaderBar = styled.header`
   width: 100vw;
   height: 8vh;
   background: ${(props) => props.theme.main};
   display: flex;
-  align-items: space-between;
+  justify-content: space-between;
   border-bottom: 3px solid ${(props) => props.theme.shadow};
 `;
 
@@ -14,14 +16,55 @@ const Logo = styled.h1`
   margin: auto;
   font-family: 'Lobster', cursive;
   color: ${(props) => props.theme.light};
+
+  &:hover {
+    color: ${(props) => props.theme.highLight};
+    cursor: pointer;
+  }
 `;
 
-const Nav = () => {
-  return (
-    <HeaderBar>
-      <Logo>Ian King</Logo>
-    </HeaderBar>
-  );
+const Brand = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+`;
+
+const Links = styled.div`
+  display: flex;
+  width: 50%;
+  align-items: center;
+  margin-right: 20px;
+`;
+
+const NavLink = styled(Link)`
+  margin: auto;
+  text-decoration: none;
+  font-family: Lobster;
+  font-size: 2em;
+  color: ${(props) => props.theme.light};
+
+  &:hover {
+    color: ${(props) => props.theme.highLight};
+  }
+`;
+
+const Nav = ({ history }) => (
+  <HeaderBar>
+    <Brand>
+      <Logo onClick={() => history.push('/')}>ᶘ ᵒᴥᵒᶅ Code Bear</Logo>
+    </Brand>
+    <Links>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/projects">Projects</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </Links>
+  </HeaderBar>
+);
+
+Nav.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
-export default Nav;
+export default withRouter(Nav);
