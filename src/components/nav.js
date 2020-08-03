b31733f0991f7a5dfb423f09d6a834b6c18bd52f
ian-antking <ian.antking@gmail.com>
2020-08-03 13:56:36 +0100

    simplify Nav component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,38 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const HeaderBar = styled.header`
+const HeaderBar = styled.nav`
   width: 100vw;
   height: 8vh;
   background: ${(props) => props.theme.main};
   display: flex;
   justify-content: space-between;
   border-bottom: 3px solid ${(props) => props.theme.shadow};
-`;
-
-const Logo = styled.h1`
-  margin: auto;
-  color: ${(props) => props.theme.light};
-
-  &:hover {
-    color: ${(props) => props.theme.highLight};
-    cursor: pointer;
-  }
-`;
-
-const Brand = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 20px;
-`;
-
-const Links = styled.div`
-  display: flex;
-  width: 50%;
-  align-items: center;
-  margin-right: 20px;
 `;
 
 const NavLink = styled(Link)`
@@ -46,23 +22,12 @@ const NavLink = styled(Link)`
   }
 `;
 
-const Nav = ({ history }) => (
+const Nav = () => (
   <HeaderBar>
-    <Brand>
-      <Logo onClick={() => history.push('/')}>ᶘ ᵒᴥᵒᶅ Code Bear</Logo>
-    </Brand>
-    <Links>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-    </Links>
+    <NavLink to="/about">About</NavLink>
+    <NavLink to="/projects">Projects</NavLink>
+    <NavLink to="/contact">Contact</NavLink>
   </HeaderBar>
 );
 
-Nav.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
-};
-
-export default withRouter(Nav);
+export default Nav;
