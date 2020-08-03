df1295d820427bd64aa93b9ec09077dfe4e22e1b
ian-antking <ian.antking@gmail.com>
2020-08-03 15:59:27 +0100

    Updates

---
+++ precache-manifest.0491cb866a19ad016aa7e319d6144d8a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a279492287ae34ec328e3ae29072ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4bcfd5c3a9e1ac8268",
+    "url": "/static/js/2.d44142e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d44142e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692730cf9d0d738cdf29",
+    "url": "/static/js/main.73b9783f.chunk.js"
+  },
+  {
+    "revision": "d09a0a3e883f3ffd3486",
+    "url": "/static/js/runtime-main.5000de7a.js"
+  },
+  {
+    "revision": "0589ea93433b4850338f4ba8fd10ed2c",
+    "url": "/static/media/amazonwebservices-original-wordmark.0589ea93.svg"
+  },
+  {
+    "revision": "564f474fba22eb0fa9b67da2d3a4b701",
+    "url": "/static/media/clojure-original.564f474f.svg"
+  },
+  {
+    "revision": "2ca884530d6e08f8540700e76c3a63cb",
+    "url": "/static/media/css3-original-wordmark.2ca88453.svg"
+  },
+  {
+    "revision": "86726983eb6e4a316184acdcf158228e",
+    "url": "/static/media/docker-original-wordmark.86726983.svg"
+  },
+  {
+    "revision": "f1a0b0e9c7eb8a3294e84f11441de5df",
+    "url": "/static/media/express-original-wordmark.f1a0b0e9.svg"
+  },
+  {
+    "revision": "66e06c06b4b40acff6a6595e710b5006",
+    "url": "/static/media/git-original-wordmark.66e06c06.svg"
+  },
+  {
+    "revision": "fff793948eaad4a21b6fb5712888d62d",
+    "url": "/static/media/github-original-wordmark.fff79394.svg"
+  },
+  {
+    "revision": "14f5e44e42eb9f47c88e209587fcfc44",
+    "url": "/static/media/gradle-plain-wordmark.14f5e44e.svg"
+  },
+  {
+    "revision": "bc02d2eef33eb10921c7a67ac231de4c",
+    "url": "/static/media/header-image.bc02d2ee.jpg"
+  },
+  {
+    "revision": "6fd6c3b7084c6789f79c57fc4d9c7a43",
+    "url": "/static/media/heroku-plain-wordmark.6fd6c3b7.svg"
+  },
+  {
+    "revision": "eb0bfe64ddcaa0b4c3be19b19541b584",
+    "url": "/static/media/html5-original-wordmark.eb0bfe64.svg"
+  },
+  {
+    "revision": "e6d87e2dca08c953fe3acd1275ed4f4e",
+    "url": "/static/media/java-original-wordmark.e6d87e2d.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "/static/media/javascript-original.de79c8f1.svg"
+  },
+  {
+    "revision": "1478533601c8d628375a8cbb63c3d84a",
+    "url": "/static/media/mocha-plain.14785336.svg"
+  },
+  {
+    "revision": "b1847af9e7cf00cbe2d44e9fa173d281",
+    "url": "/static/media/mongodb-original-wordmark.b1847af9.svg"
+  },
+  {
+    "revision": "c02ade79e7034732a856b802471a5f31",
+    "url": "/static/media/mysql-original-wordmark.c02ade79.svg"
+  },
+  {
+    "revision": "ccb43d3400fb05951c89ad3ba6acd9d5",
+    "url": "/static/media/nodejs-original.ccb43d34.svg"
+  },
+  {
+    "revision": "e83e60b310396562d703c9ccff616b91",
+    "url": "/static/media/npm-original-wordmark.e83e60b3.svg"
+  },
+  {
+    "revision": "3b84eb4026f0c704e7e3d16972bb0e3d",
+    "url": "/static/media/python-original.3b84eb40.svg"
+  },
+  {
+    "revision": "fadd9c9566ccc46c6b0e73caca5860de",
+    "url": "/static/media/react-original-wordmark.fadd9c95.svg"
+  },
+  {
+    "revision": "88c1047792044b547d1d3e066d3e18c4",
+    "url": "/static/media/sass-original.88c10477.svg"
+  },
+  {
+    "revision": "de61840db589315bf5351360614911bf",
+    "url": "/static/media/sequelize-original-wordmark.de61840d.svg"
+  },
+  {
+    "revision": "5f95aec53410e4553a86801e92cdc2fe",
+    "url": "/static/media/slack-original-wordmark.5f95aec5.svg"
+  },
+  {
+    "revision": "adbb6ec992c2c60344794f9148fe205e",
+    "url": "/static/media/vim-original.adbb6ec9.svg"
+  },
+  {
+    "revision": "bf499811c5a5f9ea4b2107a97114ff3d",
+    "url": "/static/media/webpack-original-wordmark.bf499811.svg"
+  }
+]);
