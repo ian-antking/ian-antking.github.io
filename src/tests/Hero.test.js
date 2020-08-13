6bf80ee7b761c4cea821eefba361b9b2ba03ef68
ian-antking <ian.antking@gmail.com>
2020-08-13 14:49:43 +0100

    pass angle to Hero in snapshot to prevent random behaviour breaking test

--- src/tests/Hero.test.js
+++ src/tests/Hero.test.js
@@ -5,9 +5,18 @@ import Hero from '../components/Hero';
 describe('Hero', () => {
   it('renders the correct snapshot', () => {
     const { asFragment } = render(
-      <Hero title="test-title" subtitle="test-subtitle" />
+      <Hero title="test-title" subtitle="test-subtitle" angle={60} />
     );
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders the correct title and subtitle', () => {
+    const { getByText } = render(
+      <Hero title="test-title" subtitle="test-subtitle" />
+    );
+
+    expect(getByText('test-title')).toBeInTheDocument();
+    expect(getByText('test-subtitle')).toBeInTheDocument();
   });
 });
