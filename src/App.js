f1d29e0df9a67dfcaee2504730fe284029ec12d6
ian-antking <ian.antking@gmail.com>
2020-08-14 14:09:08 +0100

    render UnderConstruction page in App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import themes from './themes';
 import Home from './pages/Home';
 import Nav from './components/Nav';
 import Page404 from './pages/404';
+import UnderConstruction from './pages/UnderConstruction';
 
 const AppContainer = styled.div`
   text-align: center;
@@ -21,9 +22,9 @@ function App() {
         <GlobalStyle />
         <Nav />
         <Switch>
-          <Route path="/about" component={() => <h1>About</h1>} />
-          <Route path="/projects" component={() => <h1>Projects</h1>} />
-          <Route path="/contact" component={() => <h1>Contact</h1>} />
+          <Route path="/about" component={() => <UnderConstruction />} />
+          <Route path="/projects" component={() => <UnderConstruction />} />
+          <Route path="/contact" component={() => <UnderConstruction />} />
           <Route path="/404" component={() => <Page404 />} />
           <Route path="/" exact component={Home} />
           <Route path="/*" component={() => <Redirect to="/404" />} />
