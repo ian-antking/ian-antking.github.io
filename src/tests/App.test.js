c3509d79a66e3845bbf5de80043ecde636e19cd2
ian-antking <ian.antking@gmail.com>
2020-08-17 09:16:07 +0100

    add snapshot test for App

---
+++ src/tests/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('App', () => {
+  let history;
+
+  beforeEach(() => {
+    history = createMemoryHistory();
+  });
+
+  it('renders the correct snapshot', () => {
+    const { asFragment } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
